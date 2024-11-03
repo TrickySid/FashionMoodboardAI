0b1b39024f20655e0b7c38521e6c20fd91834157
Siddhesh Bakre <siddheshbakre46@gmail.com>
2024-11-02 17:43:19 -0700

    Update

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -4,29 +4,23 @@ import "../styles/Login.css";
 
 function Login() {
   return (
-    <div
-      className="d-flex justify-content-center align-items-center vh-100"
-      style={{ backgroundColor: "#f3e8ff" }}
-    >
-      <div
-        className="card p-4 text-center"
-        style={{ width: "400px", borderRadius: "15px" }}
-      >
-        <h1 className="mb-4" style={{ fontWeight: "bold" }}>
-          Fashion Moodboard AI
-        </h1>
+    <div className="login-page d-flex justify-content-center align-items-center vh-100">
+      <div className="card p-4 text-center">
+        <h1 className="title mb-4">Fashion Moodboard AI</h1>
 
         {/* Login with Facebook Button */}
-        <button
-          className="btn btn-dark btn-block w-100 mb-3"
-          style={{
-            fontWeight: "bold",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <i className="bi bi-facebook me-2"></i> Login with Facebook
+        <button className="login-btn btn btn-dark btn-block w-100 mb-3">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            fill="currentColor"
+            class="bi bi-facebook"
+            viewBox="0 0 16 16"
+          >
+            <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
+          </svg>
+          <span>Login with Facebook</span>
         </button>
 
         {/* Separator */}

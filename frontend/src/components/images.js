38bc269fe90d7b47e801d4f5f6bb83c22f9a8004
Siddhesh Bakre <siddheshbakre46@gmail.com>
2024-11-08 23:06:57 -0800

    Update

--- frontend/src/components/images.js
+++ frontend/src/components/images.js
@@ -1,6 +1,6 @@
 export const images = [
   {
-    path: "/assets/img7.jpg",
+    path: "/assets/img1.jpg",
     position: { top: "-2%", left: "1%" },
     speed: 0.08,
   },
@@ -10,7 +10,7 @@ export const images = [
     speed: 0.03,
   },
   {
-    path: "/assets/img5.jpg",
+    path: "/assets/img3.jpg",
     position: { top: "25%", left: "-8%" },
     speed: 0.07,
   },
@@ -20,27 +20,27 @@ export const images = [
     speed: 0.065,
   },
   {
-    path: "/assets/img3.jpg",
+    path: "/assets/img5.jpg",
     position: { top: "75%", left: "5%" },
     speed: 0.04,
   },
   {
-    path: "/assets/img9.jpg",
+    path: "/assets/img6.jpg",
     position: { top: "-10%", left: "47%" },
     speed: 0.025,
   },
   {
-    path: "/assets/img6.jpg",
+    path: "/assets/img7.jpg",
     position: { top: "15%", left: "90%" },
     speed: 0.025,
   },
   {
-    path: "/assets/img1.jpg",
+    path: "/assets/img8.jpg",
     position: { top: "25%", left: "65%" },
     speed: 0.03,
   },
   {
-    path: "/assets/img8.jpg",
+    path: "/assets/img9.jpg",
     position: { top: "80%", left: "80%" },
     speed: 0.04,
   },

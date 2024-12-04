0527c906826be1b31bc1c7c283bd9d85660a93ae
Siddhesh Bakre <85791341+TrickySid@users.noreply.github.com>
2024-12-04 02:22:28 -0800

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="52814765259-iik4mbjput3qocfos4gbuiujsm352p8u.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
+    ,
   </React.StrictMode>
 );
